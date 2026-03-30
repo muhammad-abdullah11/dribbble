3cb585cc48714ff63b4eef8054c6525adb0a4ab1
dev-abdullah11 <abdullahworld111@gmail.com>
2026-03-30 08:44:52 +0500

    feat: add mongoose dependency and implement User model with password hashing

---
+++ models/user.model.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+const UserSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, "Full name is required"]
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    avatarUrl: {
+        type: String,
+    },
+    forgetOtp: {
+        type: String,
+    },
+    forgetOtpExpiry: {
+        type: Date,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isProfileCompleted: {
+        type: Boolean,
+        default: false
+    }
+
+}, { timestamps: true })
+
+UserSchema.pre("save", async function () {
+    if (this.isModified("password"))
+        this.password = await bcrypt.hash(this.password, 10);
+})
+
+UserSchema.methods.comparePassword = async function (password: string) {
+    return await bcrypt.compare(password, this.password);
+}
+
+export const User = mongoose.models.User || mongoose.model("User", UserSchema);
