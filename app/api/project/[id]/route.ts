928c65e8218a8113ea6be68147c723415e0e34ac
dev-abdullah11 <abdullahworld111@gmail.com>
2026-03-30 10:44:46 +0500

    feat: implement DELETE API handlers for project resource by ID by auth check

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { Project } from "@/models/project.model";
 import dbConnect from "@/utils/dbConnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/nextAuth";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
     try {
@@ -17,3 +19,21 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
     }
 }
 
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    try {
+        await dbConnect();
+        const { id } = await params;
+        const project = await Project.findByIdAndDelete(id);
+        if (!project) {
+            return NextResponse.json({ success: false, error: "Project not found" }, { status: 404 });
+        }
+        return NextResponse.json({ success: true, message: "Project deleted successfully" });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ success: false, error: "Failed to delete project" }, { status: 500 });
+    }
+}
